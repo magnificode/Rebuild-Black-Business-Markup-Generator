6e38dab8ce5086f090159fe0bca5d5c5b3968f28
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-07 09:00:53 -0700

    Linting fixes, globalStyle variable called properly within function.

--- src/styles/base/variables.js
+++ src/styles/base/variables.js
@@ -3,7 +3,7 @@ export const white = '#fff'
 
 export const colors = {
   black,
-  white
+  white,
 }
 
 export const bpTiny = 'min-width: 25em' /* 400px */
@@ -11,12 +11,11 @@ export const bpSmall = 'min-width: 40.625em' /* 650px */
 export const bpMedium = 'min-width: 53.125em' /* 850px */
 export const bpLarge = 'min-width: 62.5em' /* 1000px */
 
-
 export const mq = {
   bpTiny,
   bpSmall,
   bpMedium,
-  bpLarge
+  bpLarge,
 }
 
 export const containerWidth = '85.333em' /* 1024px */
--- src/styles/global/layout.js
+++ src/styles/global/layout.js
@@ -3,6 +3,6 @@ import { container } from '../base/variables'
 
 export const StyledContainer = styled.div`
   margin: 0 auto;
-  max-width: ${ container.width };
+  max-width: ${container.width};
   padding: 0 1rem;
 `
