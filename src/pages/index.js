9f246fe2f8dbb03caf92f3b897c4c1562eae8e38
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-07 09:01:33 -0700

    Add HelloWorld component to the index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Layout from '../theme/layout'
+import HelloWorld from '../components/HelloWorld'
 
 import { StyledContainer } from '../styles/global/layout'
 
 export default () => (
-  <Layout>
-    <StyledContainer>Hello world!</StyledContainer>
-  </Layout>
+	<Layout>
+		<StyledContainer>
+			<HelloWorld />
+		</StyledContainer>
+	</Layout>
 )
